a5d0f4772caa3b6fa81a32d2189e4d616c125076
wermisek <wermisek123@gmail.com>
2024-11-18 15:10:27 +0100

    ogarniecie logowania

---
+++ lib/adminhome.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AdminHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Admin Home")),
+      body: Center(child: Text("Welcome to the Admin Home Page!")),
+    );
+  }
+}
