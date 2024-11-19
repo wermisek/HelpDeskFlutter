c18188f3c229f3a4ec2e09b91b73a0de832e5c14
wermisek123@gmail.com  wermisek <wiktordluzniewski@gmail.com>
2024-11-19 13:15:26 +0100

    dodanie 2 usera zmienienie calego ui i w ogole sigma to tera wyglada

--- lib/login.dart
+++ lib/login.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
-import 'add_problem_page.dart'; // Dodaj stronę, gdzie problemy są dodawane
-import 'admin_home_page.dart'; // Dodaj stronę główną dla admina
+import 'package:flutter/animation.dart';
+import 'add_problem_page.dart';
+import 'admin_home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  final bool isDarkMode = false; // Możesz przełączać tryb ciemny tutaj
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool isDarkMode = false;
 
   @override
   Widget build(BuildContext context) {
@@ -17,21 +23,38 @@ class MyApp extends StatelessWidget {
           ? ThemeData.dark().copyWith(
         primaryColor: Colors.blueAccent,
         scaffoldBackgroundColor: Colors.black,
+        buttonTheme: ButtonThemeData(buttonColor: Colors.white),
+        textTheme: TextTheme(bodyMedium: TextStyle(color: Colors.white)),
       )
           : ThemeData.light().copyWith(
         primaryColor: Colors.blueAccent,
         scaffoldBackgroundColor: Colors.white,
+        buttonTheme: ButtonThemeData(buttonColor: Colors.black),
+        textTheme: TextTheme(bodyMedium: TextStyle(color: Colors.black)),
       ),
-      home: LoginPage(),
+      home: LoginPage(
+        isDarkMode: isDarkMode,
+        onThemeChanged: (value) {
+          setState(() {
+            isDarkMode = value;
+          });
+        },
+      ),
     );
   }
 }
 
-class LoginPage extends StatelessWidget {
-  final bool isDarkMode = false;
+class LoginPage extends StatefulWidget {
+  final bool isDarkMode;
+  final Function(bool) onThemeChanged;
 
-  LoginPage({Key? key}) : super(key: key);
+  LoginPage({required this.isDarkMode, required this.onThemeChanged});
 
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> with TickerProviderStateMixin {
   final _usernameController = TextEditingController();
   final _passwordController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
@@ -39,21 +62,53 @@ class LoginPage extends StatelessWidget {
   final String correctUsername = 'admin';
   final String correctPassword = 'hasło';
 
+  // Added user2 and password2
+  final String correctUsername2 = 'user2';
+  final String correctPassword2 = 'password2';
+
+  late AnimationController _controller;
+  late Animation<double> _animation;
+  late AnimationController _buttonController;
+  late Animation<Offset> _buttonAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 100),
+      vsync: this,
+    );
+    _animation = Tween<double>(begin: 0.0, end: 1.0).animate(_controller)
+      ..addListener(() {
+        setState(() {});
+      });
+    _controller.forward(); // Start the animation
+
+    // Animation for the theme switch circle
+    _buttonController = AnimationController(
+      duration: const Duration(milliseconds: 300),
+      vsync: this,
+    );
+    _buttonAnimation = Tween<Offset>(begin: Offset.zero, end: Offset(0.5, 0)).animate(
+      CurvedAnimation(parent: _buttonController, curve: Curves.easeInOut),
+    );
+  }
+
   void _login(BuildContext context) {
     if (_formKey.currentState?.validate() ?? false) {
       String username = _usernameController.text;
       String password = _passwordController.text;
 
       if (username == correctUsername && password == correctPassword) {
-        // Zamiast pokazywać tekst powitalny, przechodzimy bezpośrednio do AdminHomePage
         Navigator.pushReplacement(
           context,
           MaterialPageRoute(
             builder: (context) => AdminHomePage(),
           ),
         );
-      } else if (username == 'user' && password == 'password') {
-        // Login user, go to AddProblemPage
+      } else if ((username == 'user' && password == 'password') ||
+          (username == correctUsername2 && password == correctPassword2)) {
+        // Allow user2 to log in as a regular user
         Navigator.pushReplacement(
           context,
           MaterialPageRoute(
@@ -61,7 +116,6 @@ class LoginPage extends StatelessWidget {
           ),
         );
       } else {
-        // Invalid login, show error dialog
         showDialog(
           context: context,
           builder: (context) => AlertDialog(
@@ -82,54 +136,149 @@ class LoginPage extends StatelessWidget {
   }
 
   @override
+  void dispose() {
+    _controller.dispose();
+    _buttonController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Logowanie do HelpDesk'),
+        actions: [
+          // Animated button for switching themes (just the inner circle)
+          AnimatedAlign(
+            alignment: widget.isDarkMode ? Alignment.centerRight : Alignment.centerLeft,
+            duration: const Duration(milliseconds: 300),
+            curve: Curves.easeInOut,
+            child: GestureDetector(
+              onTap: () {
+                widget.onThemeChanged(!widget.isDarkMode);
+              },
+              child: Container(
+                width: 50,
+                height: 30,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: widget.isDarkMode ? Colors.blueAccent : Colors.grey,
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(5),
+                  child: AnimatedAlign(
+                    alignment: widget.isDarkMode ? Alignment.centerRight : Alignment.centerLeft,
+                    duration: const Duration(milliseconds: 300),
+                    curve: Curves.easeInOut,
+                    child: Container(
+                      width: 20,
+                      height: 20,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(20.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                TextFormField(
-                  controller: _usernameController,
-                  decoration: InputDecoration(
-                    labelText: 'Nazwa użytkownika',
+          child: AnimatedOpacity(
+            opacity: _animation.value,
+            duration: const Duration(seconds: 2),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  // TextField for username with icon
+                  _buildTextField(
+                    controller: _usernameController,
+                    label: 'Nazwa użytkownika',
+                    icon: Icons.person,
                   ),
-                  validator: (value) {
-                    if (value?.isEmpty ?? true) {
-                      return 'Proszę podać nazwę użytkownika';
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(height: 20),
-                TextFormField(
-                  controller: _passwordController,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'Hasło',
+                  SizedBox(height: 20),
+
+                  // TextField for password with icon
+                  _buildTextField(
+                    controller: _passwordController,
+                    label: 'Hasło',
+                    obscureText: true,
+                    icon: Icons.lock,
                   ),
-                  validator: (value) {
-                    if (value?.isEmpty ?? true) {
-                      return 'Proszę podać hasło';
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(height: 40),
-                ElevatedButton(
-                  onPressed: () => _login(context),
-                  child: Text('Zaloguj się'),
-                ),
-              ],
+                  SizedBox(height: 40),
+
+                  // Login Button with color based on theme
+                  ElevatedButton(
+                    onPressed: () => _login(context),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: widget.isDarkMode ? Colors.white : Colors.black, // Change button color
+                      padding: EdgeInsets.symmetric(vertical: 15, horizontal: 40),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                    child: Text(
+                      'Zaloguj się',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                        color: widget.isDarkMode ? Colors.black : Colors.white, // Change text color
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
+      ),
+    );
+  }
+
+  // Reusable TextField widget for username and password with icon
+  Widget _buildTextField({
+    required TextEditingController controller,
+    required String label,
+    bool obscureText = false,
+    required IconData icon,
+  }) {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 50),
+      decoration: BoxDecoration(
+        color: Colors.white70,
+        borderRadius: BorderRadius.circular(15),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.3),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      child: TextFormField(
+        controller: controller,
+        obscureText: obscureText,
+        decoration: InputDecoration(
+          labelText: label,
+          labelStyle: TextStyle(color: Colors.black),
+          border: InputBorder.none,
+          contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+          prefixIcon: Icon(icon, color: Colors.black), // Add icon
+        ),
+        style: TextStyle(color: Colors.black), // Set text color to black
+        validator: (value) {
+          if (value?.isEmpty ?? true) {
+            return 'Proszę podać $label';
+          }
+          return null;
+        },
       ),
     );
   }
