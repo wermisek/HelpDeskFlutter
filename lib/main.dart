735cfb6ff91ec9fd12dd9981435e963057f4c460
dzbanejro <cwilprzemyslaw@gmail.com>
2024-11-21 10:21:28 +0100

    window size locked. No fullscreen. (to discuss (im scared of what ive done but ill push it anyway :P (oh silly me)))

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:window_manager/window_manager.dart';
 import 'login.dart';
 
-void main(){
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+
+  windowManager.setMaximizable(false);
+  windowManager.setResizable(false);
+
   runApp(MyApp());
 }
+
