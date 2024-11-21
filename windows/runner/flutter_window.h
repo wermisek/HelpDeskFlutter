957c38938074b28465a63b0081ab49d286b8a7f8
dzbanejro <cwilprzemyslaw@gmail.com>
2024-11-21 09:59:14 +0100

    new title, bar colour (but i think i could change it later) and minsize of app (have to change maxsize)

--- windows/runner/flutter_window.h
+++ windows/runner/flutter_window.h
@@ -1,33 +1,27 @@
-#ifndef RUNNER_FLUTTER_WINDOW_H_
-#define RUNNER_FLUTTER_WINDOW_H_
+#ifndef FLUTTER_WINDOW_H_
+#define FLUTTER_WINDOW_H_
 
 #include <flutter/dart_project.h>
 #include <flutter/flutter_view_controller.h>
 
-#include <memory>
-
 #include "win32_window.h"
 
-// A window that does nothing but host a Flutter view.
 class FlutterWindow : public Win32Window {
- public:
-  // Creates a new FlutterWindow hosting a Flutter view running |project|.
-  explicit FlutterWindow(const flutter::DartProject& project);
-  virtual ~FlutterWindow();
+public:
+    explicit FlutterWindow(const flutter::DartProject& project);
+    virtual ~FlutterWindow();
 
- protected:
-  // Win32Window:
-  bool OnCreate() override;
-  void OnDestroy() override;
-  LRESULT MessageHandler(HWND window, UINT const message, WPARAM const wparam,
-                         LPARAM const lparam) noexcept override;
+protected:
+    bool OnCreate() override;
+    void OnDestroy() override;
+    LRESULT MessageHandler(HWND hwnd, UINT const message, WPARAM const wparam,
+                           LPARAM const lparam) noexcept override;
 
- private:
-  // The project to run.
-  flutter::DartProject project_;
+    void UpdateTitleBarColor(HWND hwnd); // Deklaracja funkcji
 
-  // The Flutter instance hosted by this window.
-  std::unique_ptr<flutter::FlutterViewController> flutter_controller_;
+private:
+    flutter::DartProject project_;
+    std::unique_ptr<flutter::FlutterViewController> flutter_controller_;
 };
 
-#endif  // RUNNER_FLUTTER_WINDOW_H_
+#endif  // FLUTTER_WINDOW_H_
--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,10 +27,11 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"desktop_help_flutter", origin, size)) {
+  if (!window.Create(L"HelpDesk Drzewniak", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
+
 
   ::MSG msg;
   while (::GetMessage(&msg, nullptr, 0, 0)) {
