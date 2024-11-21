ae07403bb26da535982e32eac0f02c9cbe340322
wermisek <wiktordluzniewski@gmail.com>
2024-11-21 12:23:17 +0100

    Po dlugich ciezkich bataliach wreszcie strona admina wyglada superowo

--- lib/admin_home_page.dart
+++ lib/admin_home_page.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'dart:async';
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'login.dart';  // Import strony logowania
+import 'login.dart'; // Import strony logowania
 
 void main() {
   runApp(MyApp());
@@ -13,11 +13,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'HelpDesk Admin',
-      theme: ThemeData.dark().copyWith(
-        primaryColor: Colors.blueAccent,
-        scaffoldBackgroundColor: Colors.black,
+      theme: ThemeData.light().copyWith(
+        primaryColor: Color(0xFFF5F5F5), // Set the primary color to #f5f5f5
+        scaffoldBackgroundColor: Color(0xFFF5F5F5), // Set the scaffold background color to #f5f5f5
         buttonTheme: ButtonThemeData(buttonColor: Colors.white),
-        textTheme: TextTheme(bodyMedium: TextStyle(color: Colors.white)),
+        textTheme: TextTheme(bodyMedium: TextStyle(color: Colors.black)), // Set text color to black
       ),
       home: AdminHomePage(),
     );
@@ -33,8 +33,6 @@ class _AdminHomePageState extends State<AdminHomePage> {
   List<dynamic> unreadProblems = [];
   List<dynamic> readProblems = [];
   Timer? _refreshTimer;
-  ScrollController _unreadScrollController = ScrollController();
-  ScrollController _readScrollController = ScrollController();
 
   Future<void> getProblems() async {
     try {
@@ -46,10 +44,10 @@ class _AdminHomePageState extends State<AdminHomePage> {
 
       setState(() {
         unreadProblems = fetchedProblems
-            .where((p) => (p['read'] ?? false) == 0)  // Assuming 0 means unread
+            .where((p) => (p['read'] ?? false) == 0) // Assuming 0 means unread
             .toList();
         readProblems = fetchedProblems
-            .where((p) => (p['read'] ?? false) == 1)  // Assuming 1 means read
+            .where((p) => (p['read'] ?? false) == 1) // Assuming 1 means read
             .toList();
       });
     } catch (e) {
@@ -101,10 +99,58 @@ class _AdminHomePageState extends State<AdminHomePage> {
   }
 
   void _logout() {
-    // Tutaj należy dodać logikę wylogowania, np. usuwając dane logowania z pamięci.
     Navigator.pushReplacement(
       context,
-      MaterialPageRoute(builder: (context) => LoginPage()),  // Przenosimy użytkownika na stronę logowania
+      MaterialPageRoute(builder: (context) => LoginPage()), // Przenosimy użytkownika na stronę logowania
+    );
+  }
+
+  void _showPopup(BuildContext context, dynamic problem) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        backgroundColor: Color(0xFFF5F5F5), // Jaśniejszy kolor tła popupu
+        contentPadding: EdgeInsets.all(20),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        content: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Zgłoszenie od: ${problem['username'] ?? 'Nieznany'}',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Pokój: ${problem['room'] ?? 'Nieznany'}',
+                style: TextStyle(fontSize: 14),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Opis problemu:',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+              ),
+              SizedBox(height: 5),
+              Text(
+                problem['problem'] ?? 'Brak opisu',
+                style: TextStyle(fontSize: 14),
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: Text('Zamknij', style: TextStyle(color: Colors.blue)),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 
@@ -113,178 +159,162 @@ class _AdminHomePageState extends State<AdminHomePage> {
     super.initState();
     getProblems();
     _refreshTimer = Timer.periodic(Duration(seconds: 5), (timer) {
-      getProblems();  // Fetch problems every 5 seconds
+      getProblems(); // Fetch problems every 5 seconds
     });
   }
 
   @override
   void dispose() {
     _refreshTimer?.cancel();
-    _unreadScrollController.dispose();
-    _readScrollController.dispose();
     super.dispose();
+  }
+
+  Widget _buildProblemGrid(String title, List<dynamic> problems, bool isUnread) {
+    return Expanded(
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0), // Zwiększenie zewnętrznego marginesu
+        padding: EdgeInsets.only(left: 5.0, right: 5.0, top: 8.0, bottom: 8.0), // Padding wewnętrzny
+        decoration: BoxDecoration(
+          color: Color(0xFFF5F5F5), // Background color changed to #f5f5f5
+          border: Border.all(color: Colors.black, width: 3), // Szerszy border
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            Expanded(
+              child: problems.isEmpty
+                  ? Center(
+                child: Text(
+                  isUnread
+                      ? 'Brak nieodczytanych zgłoszeń.'
+                      : 'Brak odczytanych zgłoszeń.',
+                  style: TextStyle(fontSize: 14.0, color: Colors.black),
+                ),
+              )
+                  : GridView.builder(
+                padding: EdgeInsets.symmetric(vertical: 8.0),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4, // Four tiles per row
+                  crossAxisSpacing: 8.0,
+                  mainAxisSpacing: 8.0,
+                  childAspectRatio: 1.8, // Adjust aspect ratio for compact tiles
+                ),
+                itemCount: problems.length,
+                itemBuilder: (context, index) {
+                  var problem = problems[index];
+                  return Container(
+                    decoration: BoxDecoration(
+                      color: Colors.black, // Tło bardzo ciemne, prawie czarne
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Username: ${problem['username'] ?? 'Nieznany'}',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 14), // Decreased font size
+                              ),
+                              IconButton(
+                                icon: Icon(Icons.arrow_forward_ios, size: 18, color: Colors.white),
+                                onPressed: () {
+                                  _showPopup(context, problem); // Show full problem in popup
+                                },
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 4),
+                          Text(
+                            'Room: ${problem['room'] ?? 'Nieznany'}',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 14), // Decreased font size
+                          ),
+                          SizedBox(height: 4),
+                          Text(
+                            'Problem: ${problem['problem'] ?? 'Brak opisu'}',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                          Spacer(),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              IconButton(
+                                icon: Icon(Icons.delete, color: Colors.red),
+                                onPressed: () {
+                                  deleteProblem(problem['id']);
+                                },
+                              ),
+                              IconButton(
+                                icon: Icon(Icons.check, color: Colors.green),
+                                onPressed: () {
+                                  markAsRead(problem['id']);
+                                },
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Admin Home',
-          style: TextStyle(color: Colors.black),  // Text color black
-        ),
-        backgroundColor: Colors.white,  // AppBar background white
-        elevation: 0,
-        actions: [
-          IconButton(
-            icon: Icon(Icons.refresh, color: Colors.black),  // Refresh icon color black
-            onPressed: getProblems,
+      backgroundColor: Color(0xFFF5F5F5),
+      body: Column(
+        children: [
+          Container(
+            height: 60,
+            color: Color(0xFFF5F5F5), // Background color of the header
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    'Panel administratora',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-          IconButton(
-            icon: Icon(Icons.exit_to_app, color: Colors.black),  // Wyloguj ikona
-            onPressed: _logout,  // Wywołanie funkcji wylogowania
-          ),
+          _buildProblemGrid('Nieodczytane Zgłoszenia', unreadProblems, true),
+          _buildProblemGrid('Odczytane Zgłoszenia', readProblems, false),
         ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Nieodczytane zgłoszenia',
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black),  // Text color black
-                  ),
-                  Expanded(
-                    child: unreadProblems.isEmpty
-                        ? Center(
-                      child: Text(
-                        'Brak nieodczytanych zgłoszeń.',
-                        style: TextStyle(fontSize: 16.0, color: Colors.black),  // Text color black
-                      ),
-                    )
-                        : ListView.builder(
-                      controller: _unreadScrollController,
-                      physics: BouncingScrollPhysics(), // For smoother scrolling
-                      itemCount: unreadProblems.length,
-                      itemBuilder: (context, index) {
-                        var problem = unreadProblems[index];
-                        return Card(
-                          margin: EdgeInsets.symmetric(vertical: 8.0),
-                          color: Colors.grey[800],
-                          child: ListTile(
-                            title: Text(
-                              'Username: ${problem['username'] ?? 'Nieznany'}',
-                              style: TextStyle(color: Colors.white),
-                            ),
-                            subtitle: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  'Room: ${problem['room'] ?? 'Nieznany'}',
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                                Text(
-                                  'Problem: ${problem['problem'] ?? 'Brak opisu'}',
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                                Text(
-                                  'Timestamp: ${problem['timestamp'] ?? 'Nieznany'}',
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ],
-                            ),
-                            trailing: Row(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                IconButton(
-                                  icon: Icon(Icons.delete, color: Colors.red),
-                                  onPressed: () => deleteProblem(problem['id']),
-                                ),
-                                IconButton(
-                                  icon: Icon(Icons.check, color: Colors.green),
-                                  onPressed: () => markAsRead(problem['id']),
-                                ),
-                              ],
-                            ),
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Odczytane zgłoszenia',
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black),  // Text color black
-                  ),
-                  Expanded(
-                    child: readProblems.isEmpty
-                        ? Center(
-                      child: Text(
-                        'Brak odczytanych zgłoszeń.',
-                        style: TextStyle(fontSize: 16.0, color: Colors.black),  // Text color black
-                      ),
-                    )
-                        : ListView.builder(
-                      controller: _readScrollController,
-                      physics: BouncingScrollPhysics(), // For smoother scrolling
-                      itemCount: readProblems.length,
-                      itemBuilder: (context, index) {
-                        var problem = readProblems[index];
-                        return Card(
-                          margin: EdgeInsets.symmetric(vertical: 8.0),
-                          color: Colors.grey[800],
-                          child: ListTile(
-                            title: Text(
-                              'Username: ${problem['username'] ?? 'Nieznany'}',
-                              style: TextStyle(color: Colors.white),
-                            ),
-                            subtitle: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  'Room: ${problem['room'] ?? 'Nieznany'}',
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                                Text(
-                                  'Problem: ${problem['problem'] ?? 'Brak opisu'}',
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                                Text(
-                                  'Timestamp: ${problem['timestamp'] ?? 'Nieznany'}',
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ],
-                            ),
-                            trailing: IconButton(
-                              icon: Icon(Icons.delete, color: Colors.red),
-                              onPressed: () => deleteProblem(problem['id']),
-                            ),
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
       ),
     );
   }
