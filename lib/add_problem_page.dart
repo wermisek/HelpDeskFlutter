a2ddc2061ebf8e66fb71cb831b7ae1ffbd69c369
dzbanejro <cwilprzemyslaw@gmail.com>
2024-11-25 13:53:47 +0100

    push WIP

--- lib/add_problem_page.dart
+++ lib/add_problem_page.dart
@@ -18,81 +18,34 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class UserHomePage extends StatelessWidget {
+class UserHomePage extends StatefulWidget {
   final String username;
 
   UserHomePage({required this.username});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Zalogowano jako $username'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Witaj, $username!'),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => AddProblemPage(username: username),
-                  ),
-                );
-              },
-              child: Text('Dodaj Problem'),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => ProblemyPage(username: username),
-                  ),
-                );
-              },
-              child: Text('Moje Zgłoszenia'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+  _UserHomePageState createState() => _UserHomePageState();
 }
 
-class AddProblemPage extends StatefulWidget {
-  final String username;
-
-  AddProblemPage({required this.username});
-
-  @override
-  _AddProblemPageState createState() => _AddProblemPageState();
-}
-
-class _AddProblemPageState extends State<AddProblemPage> {
+class _UserHomePageState extends State<UserHomePage> {
+  final _teacherController = TextEditingController(text: 'Nauczyciel X'); // Pre-filled teacher
   final _roomController = TextEditingController();
   final _problemController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
-
-  bool _showForm = false;
-  bool _isOtherButtonVisible = true;
+  bool showUsers = false;
+  bool showProblems = true;
 
   Future<void> _submitProblem(BuildContext context) async {
     if (_formKey.currentState?.validate() ?? false) {
       String room = _roomController.text;
       String problem = _problemController.text;
 
-      // Dodanie parametru 'read' z wartością 0 (nieodczytane)
+      // Add 'read' parameter with value 0 (unread)
       Map<String, dynamic> problemData = {
         'username': widget.username,
         'room': room,
         'problem': problem,
-        'read': 0, // Dodanie statusu jako nieodczytane
+        'read': 0, // Status set to unread
       };
 
       try {
@@ -148,97 +101,222 @@ class _AddProblemPageState extends State<AddProblemPage> {
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Dodaj Problem'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.settings),
-            onPressed: () {
-              // Remove isDarkMode and toggleTheme parameters if they aren't used
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => SettingsPage(),
-                ),
-              );
-            },
+  Widget _buildInputField({
+    required TextEditingController controller,
+    required String labelText,
+    bool enabled = true,
+    int maxLines = 1,
+    String? Function(String?)? validator,
+  }) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            offset: Offset(0, 2), // Shadow only at the bottom
+            blurRadius: 4,
           ),
         ],
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    _showForm = !_showForm;
-                    _isOtherButtonVisible = !_isOtherButtonVisible;
-                  });
-                },
-                child: Text(_showForm ? 'Anuluj' : 'Dodaj Problem'),
-              ),
-              SizedBox(height: 20),
-              if (_isOtherButtonVisible)
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => ProblemyPage(username: widget.username),
-                      ),
-                    );
-                  },
-                  child: Text('Moje Zgłoszenia'),
-                ),
-              SizedBox(height: 20),
-              if (_showForm)
-                Form(
-                  key: _formKey,
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        controller: _roomController,
-                        decoration: InputDecoration(
-                          labelText: 'Numer Sali',
-                          border: OutlineInputBorder(),
+      child: TextFormField(
+        controller: controller,
+        maxLines: maxLines,
+        enabled: enabled,
+        decoration: InputDecoration(
+          labelText: labelText,
+          filled: true,
+          fillColor: Colors.white,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8.0),
+            borderSide: BorderSide.none, // No border
+          ),
+        ),
+        validator: validator,
+      ),
+    );
+  }
+
+  Widget _buildSubmitButton(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            offset: Offset(0, 2), // Shadow only at the bottom
+            blurRadius: 4,
+          ),
+        ],
+      ),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+          padding: EdgeInsets.symmetric(vertical: 14.0, horizontal: 24.0),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+          // Disable hover effect
+          shadowColor: Colors.transparent, // Removes the hover effect
+          elevation: 0, // Removes elevation
+        ),
+        onPressed: () => _submitProblem(context),
+        child: Text('Zgłoś problem'),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Define height of the input fields for 'Nauczyciel' and 'Pokój'
+    double problemFieldHeight = 80.0; // Height of 'Opis problemu' input
+    double inputHeight = problemFieldHeight * 0.75; // 75% of 'Opis problemu' height
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'HelpDesk Strona Główna',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        backgroundColor: Color(0xFFF5F5F5),
+        elevation: 0,
+        iconTheme: IconThemeData(color: Colors.black),
+      ),
+      body: Column(
+        children: [
+          // Divider to add line separating AppBar and the main screen
+          Container(
+            color: Color(0xFF8A8A8A),
+            height: 1.0, // Line thickness
+          ),
+          Expanded(
+            child: Container(
+              color: Color(0xFFF5F5F5),
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        // Row for 'Nauczyciel' and 'Pokój' side by side
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
+                              width: MediaQuery.of(context).size.width * 0.45, // Adjust width to fit 75% of 'Opis problemu'
+                              height: inputHeight,
+                              child: _buildInputField(
+                                controller: _teacherController,
+                                labelText: 'Nauczyciel',
+                                enabled: false,
+                              ),
+                            ),
+                            SizedBox(width: 16), // Space between the two input fields
+                            Container(
+                              width: MediaQuery.of(context).size.width * 0.45,
+                              height: inputHeight,
+                              child: _buildInputField(
+                                controller: _roomController,
+                                labelText: 'Pokój',
+                                validator: (value) {
+                                  if (value?.isEmpty ?? true) {
+                                    return 'Wprowadź pokój';
+                                  }
+                                  return null;
+                                },
+                              ),
+                            ),
+                          ],
                         ),
-                        style: TextStyle(color: Colors.black),
-                        validator: (value) {
-                          if (value?.isEmpty ?? true) {
-                            return 'Proszę podać numer sali';
-                          }
-                          return null;
-                        },
-                      ),
-                      SizedBox(height: 20),
-                      TextFormField(
-                        controller: _problemController,
-                        decoration: InputDecoration(
-                          labelText: 'Opis Problemu',
-                          border: OutlineInputBorder(),
+                        const SizedBox(height: 16.0),
+                        // Input for 'Opis problemu'
+                        _buildInputField(
+                          controller: _problemController,
+                          labelText: 'Opis problemu',
+                          maxLines: 5,
+                          validator: (value) {
+                            if (value?.isEmpty ?? true) {
+                              return 'Wprowadź opis problemu';
+                            }
+                            return null;
+                          },
                         ),
-                        style: TextStyle(color: Colors.black),
-                        validator: (value) {
-                          if (value?.isEmpty ?? true) {
-                            return 'Proszę podać opis problemu';
-                          }
-                          return null;
-                        },
-                      ),
-                      SizedBox(height: 20),
-                      ElevatedButton(
-                        onPressed: () => _submitProblem(context),
-                        child: Text('Wyślij problem'),
-                      ),
-                    ],
+                        const SizedBox(height: 24.0),
+                        _buildSubmitButton(context),
+                      ],
+                    ),
                   ),
                 ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        child: Container(
+          color: Colors.white,
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              Container(
+                height: 73.0,
+                child: UserAccountsDrawerHeader(
+                  accountName: Text(
+                    'Helpdesk Admin',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 22.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  accountEmail: null,
+                  currentAccountPicture: null,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                  ),
+                  margin: EdgeInsets.zero,
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.report_problem, color: Colors.black),
+                title: Text('Dodaj problem', style: TextStyle(color: Colors.black)),
+                onTap: () {
+                  setState(() {
+                    showProblems = true;
+                    showUsers = false;
+                  });
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.group, color: Colors.black),
+                title: Text('Moje problemy', style: TextStyle(color: Colors.black)),
+                onTap: () {
+                  setState(() {
+                    showProblems = false;
+                    showUsers = true;
+                  });
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.settings, color: Colors.black),
+                title: Text('Ustawienia', style: TextStyle(color: Colors.black)),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SettingsPage()),
+                  );
+                },
+              ),
             ],
           ),
         ),
